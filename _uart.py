56683c5abf318305b9f4cf04c873759811025703
Simon Hensel <simon.hensel@inovex.de>
2025-05-22 17:11:00 +0200

    Add Python script to interact with FPGA

---
+++ _uart.py
@@ -0,0 +1,74 @@
+import serial
+
+
+device = serial.Serial('/dev/ttyUSB1')
+device.baudrate = 115200    # set Baud rate to 9600
+device.bytesize = 8         # Number of data bits = 8
+device.parity   = 'N'       # No parity
+device.stopbits = 1         # Number of Stop bits = 1
+
+
+cmd0 = bytearray(4)
+cmd0[0] = 0
+
+data0 = bytearray(136)
+data0[0] = 2
+
+cmd1 = bytearray(4)
+cmd1[0] = 1
+
+data1 = bytearray(136)
+data1[0] = 3
+
+for i in range(4):
+    device.write(int.to_bytes(cmd0[i]))
+
+for i in range(136):
+    device.write(int.to_bytes(data0[i]))
+
+for i in range(4):
+    device.write(int.to_bytes(cmd1[i]))
+
+for i in range(136):
+    device.write(int.to_bytes(data1[i]))
+
+out = device.read(32)
+
+# the bytes within each word are in reverse order, rearrange them
+md = bytearray(32)
+for i in range(8):
+    md[4 * i + 0] = out[4 * i + 3]
+    md[4 * i + 1] = out[4 * i + 2]
+    md[4 * i + 2] = out[4 * i + 1]
+    md[4 * i + 3] = out[4 * i + 0]
+
+print(md.hex())
+
+
+# send more data after reading the first output
+cmd2 = bytearray(4)
+cmd2[0] = 1
+
+data2 = bytearray(136)
+data2[0] = 4
+
+for i in range(4):
+    device.write(int.to_bytes(cmd2[i]))
+
+for i in range(136):
+    device.write(int.to_bytes(data2[i]))
+
+out = device.read(32)
+
+# the bytes within each word are in reverse order, rearrange them
+md = bytearray(32)
+for i in range(8):
+    md[4 * i + 0] = out[4 * i + 3]
+    md[4 * i + 1] = out[4 * i + 2]
+    md[4 * i + 2] = out[4 * i + 1]
+    md[4 * i + 3] = out[4 * i + 0]
+
+print(md.hex())
+
+
+device.close()
